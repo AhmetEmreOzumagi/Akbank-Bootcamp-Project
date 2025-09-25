7bc2b425ad80b4a01ada2f8662cf1d628a4e0c2b
AhmetEmreÖzümağı <ahmet.emre2005@gmail.com>
2025-09-25 23:37:32 +0300

    Update convert_model.py

--- UI/convert_model.py
+++ UI/convert_model.py
@@ -1,1 +1,9 @@
+import tensorflow as tf
+from keras.utils import custom_object_scope
+
+with custom_object_scope({'TrueDivide': tf.keras.layers.Lambda(lambda x: x)}):
+    model = tf.keras.models.load_model("best_model.h5", compile=False)
+
+model.save("best_model_tf", save_format="tf")
+print("✅ Model başarıyla 'best_model_tf' klasörüne kaydedildi.")
 
